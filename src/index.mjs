f0eb5df3643d6ef3873f1944a27f4d1048c3a99f
iamjohnhm <jhm90822@gmail.com>
2024-07-23 12:01:03 +0300

    Routes

--- src/index.mjs
+++ src/index.mjs
@@ -1,41 +1,12 @@
 import express from "express";
-import { query, validationResult, body, matchedData, checkSchema } from "express-validator";
-import {createUserValidationSchema} from './utils/validationSchemas.mjs'
+import routes from "./routes/index.mjs"
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(routes);
 
-const loggingMiddleware = (req, res, next) => {
-    console.log(`${req.method} - ${req.url}`);
-    next();
-}
-
-const resolveIndexByUserId = ( req, res, next) => {
-    const { body, 
-        params: { id }, 
-    } = req;
-
-    const parsedId = parseInt(id);
-    if (isNaN(parsedId)) return res.sendStatus(400);
-
-    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-    
-    if (findUserIndex === -1) return res.sendStatus(404);
-    req.findUserIndex = findUserIndex;
-    next();
-}
 const PORT = process.env.PORT || 3000;
-
-const mockUsers = [
-                   { id: 1, username: "anson", displayName: "Anson"},
-                   { id: 2, username: "jack", displayName: "Jack"},
-                   { id: 3, username: "adam", displayName: "Adam"},
-                   { id: 4, username: "tina", displayName: "Tina"},
-                   { id: 5, username: "jason", displayName: "Jason"},
-                   { id: 6, username: "henry", displayName: "Henry"},
-                   { id: 7, username: "marilyn", displayName: "Marilyn"}
-                ]
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
@@ -62,74 +33,6 @@ app.get("/",
     res.status(201).send({ msg: "Hello" });
 });
 
-app.get("/api/users", query('filter')
-    .isString()
-    .withMessage("must be string")
-    .notEmpty()
-    .withMessage("Must not be empty")
-    .isLength({ min: 3, max: 10 })
-    .withMessage("Must be at least 3-10 characters"), 
-    (req,res) => {
-        const result = validationResult(req);
-        console.log(result);  
-        const {
-            query: { filter, value },
-        } = req;
-        if (filter && value) return res.send(
-            mockUsers.filter((user) => user[filter].includes(value))
-        );
 
-    return res.send(mockUsers);
-});
 
-app.use(loggingMiddleware, (req, res, next) => {
-    console.log("Finished Logging...");
-    next();
-})
-
-app.post('/api/users', 
-    checkSchema(createUserValidationSchema), 
-    (req, res) => {
-        const result = validationResult(req);
-        console.log(result);
-
-        if (!result.isEmpty())
-            return res.status(400).send({ errors: result.array() });
-        const data = matchedData(req);
-        const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body }
-        mockUsers.push(newUser);
-        return res.status(201).send(newUser);
-})
- 
-app.get("/api/users/:id", resolveIndexByUserId, (req,res) => {
-    const { findUserIndex } = req;
-    const findUser = mockUsers[findUserIndex];
-    if (!findUser) return res.sendStatus(404);
-    return res.send(findUser);
-});
-
-app.get("/api/products", (req,res) => {
-    res.send([{ id:123, name: "chicken breast", price: 12.99 }]);
-}); 
-
-app.put("/api/users/:id", resolveIndexByUserId, (req,res) => {
-    const { body, findUserIndex } = req;
-
-    mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
-    return res.sendStatus(200); 
-})
-
-app.patch('/api/users/:id', resolveIndexByUserId , (req,res) => {
-    const { body, findUserIndex } = req;
-
-    mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
-    return res.sendStatus(200);
-} )
-
-app.delete('/api/users/:id', resolveIndexByUserId, (req,res) => {
-    const { findUserIndex } = req;
-    
-    mockUsers.splice(findUserIndex, 1);
-    return res.sendStatus(200);
-})
 
