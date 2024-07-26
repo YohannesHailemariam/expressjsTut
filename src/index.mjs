bf45c815c77e22a7c1dcbc789c96f339eecd9964
iamjohnhm <jhm90822@gmail.com>
2024-07-26 12:49:05 +0300

    Session pt. 2

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./routes/index.mjs"
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { mockUsers } from "./utils/constants.mjs";
 
 const app = express();
 
@@ -32,6 +33,43 @@ app.get("/",
         res.status(201).send({ msg: "Hello" });
 });
 
+app.post('/api/auth', (req, res) => {
+    const { 
+        body: { username, password }, 
+    } = req;
+    const findUser = mockUsers.find((user) => user.username === username);
+    if (!findUser || findUser.password !== password) 
+        return res.status(401).send({ msg: 'BAD CREDENTIALS' });
+
+    req.session.user = findUser;
+    return res.status(200).send(findUser);
+})
+
+app.get('/api/auth/status', (req, res) => {
+    req.sessionStore.get(req.sessionID, (err, session) => {
+        console.log(session);
+    }); 
+    return req.session.user ? res.status(200).send(req.session.user)
+    : res.status(401).send( { msg: "Not Authenticated" } );
+})
+
+app.post("/api/cart", (req, res) => {
+    if (!req.session.user) return res.sendStatus(401);
+    const { body: item } = req;
+    const { cart } = req.session;
+    if (cart) {
+        cart.push(item);
+    }
+    else {
+        req.session.cart = [item];
+    }
+    return res.status(201).send(item);
+})
+
+app.get('/api/cart', (req, res) => {
+    if (!req.session.user) return res.sendStatus(401);
+    return res.send(req.session.cart ?? []);
+})
 
 
 
