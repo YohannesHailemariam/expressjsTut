db7327974fbbd6d0b6573cd2037eb43a01161781
iamjohnhm <jhm90822@gmail.com>
2024-08-02 14:04:50 +0300

    Created a file name discord-starategy in the strategies folder to hadle the logic that is going to be implemented to authorize users through discord

--- src/index.mjs
+++ src/index.mjs
@@ -6,7 +6,7 @@ import { mockUsers } from "./utils/constants.mjs";
 import passport from "passport";
 import mongoose from "mongoose";
 import MongoStore from "connect-mongo"
-import "./strategies/local-strategy.mjs";
+//import "./strategies/local-strategy.mjs";
 
 const app = express();
 
