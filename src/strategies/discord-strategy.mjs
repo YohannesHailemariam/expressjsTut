d44cadea4b3b1a0efd6a0ae8795941aa2671368d
iamjohnhm <jhm90822@gmail.com>
2024-08-02 14:50:29 +0300

    Handling errors that maybe thrown by the findOne and Save functions in the discord-strategy file

--- src/strategies/discord-strategy.mjs
+++ src/strategies/discord-strategy.mjs
@@ -1,5 +1,6 @@
 import passport from "passport";
 import { Strategy } from "passport-discord";
+import { DiscordUser } from "../mongoose/schemas/discord-user.mjs";
 
 export default passport.use(
     new Strategy({
@@ -8,8 +9,26 @@ export default passport.use(
         callbackURL: 'http://localhost:5000/api/auth/discord/redirect',
         scope: ["identify"],
     }, 
-    (accessToken, refreshToken, profile, done) => {
-        console.log(profile);
+    async (accessToken, refreshToken, profile, done) => {
+        let findUser;
+        try {
+            findUser = await DiscordUser.findOne({ discordId: profile.id });
+        } catch (err) {
+            return done(err, null);
+        }
+        
+        try {
+            if (!findUser) {
+                const newUser = new DiscordUser({
+                    username: profile.username,
+                    discordId: profile.id,
+                })
+                const newSavedUser = await newUser.save();
+                done(null, newSavedUser);
+            }
+        } catch (err) {
+            console.log(err);
+        }
     }
    )
 )
