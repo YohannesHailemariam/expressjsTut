bbad3e46de0cc0735c2b73f7360195d5472c7c28
iamjohnhm <jhm90822@gmail.com>
2024-08-16 06:03:35 +0300

    Testing the get method functionality inside the index file

--- src/__tests__/users.spec.js
+++ src/__tests__/users.spec.js
@@ -16,5 +16,14 @@ describe('get users', () => {
         expect(mockResponse.send).toHaveBeenCalled();
         expect(mockResponse.send).toHaveBeenCalledWith(mockUsers[1]);
         expect(mockResponse.send).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call sendStatus with 404 when user not found', () => {
+        const copyMockRequest = { ...mockRequest, findUserIndex: 100};
+        getUserByIdHandler(copyMockRequest, mockResponse);
+        expect(mockResponse.sendStatus).toHaveBeenCalled();
+        expect(mockResponse.sendStatus).toHaveBeenCalledWith(404);
+        expect(mockResponse.sendStatus).toHaveBeenCalledTimes(1);
+        expect(mockResponse.send).not.toHaveBeenCalled();
     })
 });
