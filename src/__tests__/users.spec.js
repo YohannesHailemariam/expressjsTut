203db854f5ca47898290d08311917d02d314a8bf
iamjohnhm <jhm90822@gmail.com>
2024-08-16 05:50:57 +0300

    Testing the findUserIndex and findUser section

--- src/__tests__/users.spec.js
+++ src/__tests__/users.spec.js
@@ -1,11 +1,20 @@
 const { getUserByIdHandler } = require("../handlers/users.mjs");
+const { mockUsers } = require("../utils/constants.mjs");
 
-const mockRequest = {};
+const mockRequest = {
+    findUserIndex: 1,
+};
 
-const mockResponse = {};
+const mockResponse = {
+    sendStatus: jest.fn(),
+    send: jest.fn()
+};
 
 describe('get users', () => {
     it('should get user by id', () => {
-        getUserByIdHandler()
+        getUserByIdHandler(mockRequest, mockResponse);
+        expect(mockResponse.send).toHaveBeenCalled();
+        expect(mockResponse.send).toHaveBeenCalledWith(mockUsers[1]);
+        expect(mockResponse.send).toHaveBeenCalledTimes(1);
     })
 });
